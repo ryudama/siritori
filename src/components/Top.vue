518bd81059bdff27dd67021c53ebe0458bc5c3dc
ryudama <kaihatsu.hidehiro.iida@gmail.com>
2024-01-22 20:45:10 +0900

    リレーができるまで

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -11,6 +11,7 @@ const member = ref([])
 const usedWord = ref('')
 const usedWordList = ref([])
 let currentIndex = ref(0)
+let endWord = ref('')
 
 const disqualification = ref(false)
 
@@ -19,24 +20,44 @@ const rule = ref(false)
 
 const addWord = () => {
     usedWord.value = usedWord.value.replace(/[\u30a1-\u30f6]/g, match => String.fromCharCode(match.charCodeAt(0) - 0x60))   //カタカナをひらがなに変換
-
-    //タブー設定
-    if(usedWord.value.slice(-1) === 'ん' || //ん　がついた時
-        !/^[ぁ-んー]$/u.test(usedWord.value.slice(-1)) ||    //ひらがなとー以外を言った時
-        usedWordList.value.some(word => word === usedWord.value)    //前と同じ言葉を言った時
-        ){
+    if(usedWordList.value.length === 0){    //1人目専用
+        usedWordList.value.push(usedWord.value) //使った言葉リストに格納
+        endWord.value = usedWord.value.slice(-1) //次の言葉の頭として格納
+                            .replace(/っ/g, 'つ')   //小さい文字を大きく変更
+                            .replace(/ゃ/g, 'や')
+                            .replace(/ゅ/g, 'ゆ')
+                            .replace(/ょ/g, 'よ')
+                            .replace(/ぁ/g, 'あ')
+                            .replace(/ぃ/g, 'い')
+                            .replace(/ぅ/g, 'う')
+                            .replace(/ぇ/g, 'え')
+                            .replace(/ぉ/g, 'お')
+        console.log(endWord.value)
         usedWord.value = ''
-        member.value.splice(currentIndex-1,1)
-        currentIndex.value = (currentIndex.value) % member.value.length
-        disqualification.value = true
-    }
-    else{
-        console.log('今の言ったのは' + usedWord.value)
-        usedWordList.value.push(usedWord.value)
-        usedWord.value = ''
-        console.log('今まで言ったリストは' + usedWordList.value)
         currentIndex.value = (currentIndex.value + 1) % member.value.length
-        console.log('次は'+member.value[(currentIndex.value) % member.value.length])
+    }else if(endWord.value !== usedWord.value.slice(0,1) ||  //前の文字と次の文字が合ってない
+            usedWord.value.slice(-1) === 'ん' || //ん　がついた時
+            !/^[ぁ-んー]$/u.test(usedWord.value.slice(-1)) ||    //ひらがなとー以外を言った時
+            usedWordList.value.some(word => word === usedWord.value)){    //前に言ったのと同じ言葉を言った時
+                usedWord.value = ''
+                member.value.splice(currentIndex-1,1)
+                currentIndex.value = (currentIndex.value) % member.value.length
+                disqualification.value = true
+    }else{  //続く時
+        usedWordList.value.push(usedWord.value)
+        endWord.value = usedWord.value.slice(-1) //次の言葉の頭として格納
+                            .replace(/っ/g, 'つ')   //小さい文字を大きく変更
+                            .replace(/ゃ/g, 'や')
+                            .replace(/ゅ/g, 'ゆ')
+                            .replace(/ょ/g, 'よ')
+                            .replace(/ぁ/g, 'あ')
+                            .replace(/ぃ/g, 'い')
+                            .replace(/ぅ/g, 'う')
+                            .replace(/ぇ/g, 'え')
+                            .replace(/ぉ/g, 'お')
+        console.log(endWord.value)
+        usedWord.value = ''
+        currentIndex.value = (currentIndex.value + 1) % member.value.length
     }
 }
 
